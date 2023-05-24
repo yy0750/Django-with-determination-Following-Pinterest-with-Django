bdabaa2051c802d59b5d5a86d7a19980b8f30e23
yy0750 <yywls23@gmail.com>
2023-05-24 16:07:01 +0900

    django course 32 commit

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -6,4 +6,6 @@ app_name = 'profileapp'
 
 urlpatterns = [
     path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileCreateView.as_view(), name='update'),
+
 ]
