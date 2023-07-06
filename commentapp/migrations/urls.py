3d0c0768ded1d51a80a0dde2ff5c9ea1eda06c34
yy0750 <yywls23@gmail.com>
2023-07-07 04:31:31 +0900

    django course 38 commit

--- commentapp/migrations/urls.py
+++ commentapp/migrations/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from commentapp.migrations.views import CommentCreateView
+from commentapp.migrations.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'
 
@@ -8,5 +8,6 @@ app_name = 'commentapp'
 
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
+    path('delete/<int:pk>/', CommentDeleteView.as_view(), name='delete'),
 
 ]
--- commentapp/migrations/views.py
+++ commentapp/migrations/views.py
@@ -1,9 +1,11 @@
 import self
 from commentapp.models import Comment
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
+from commentapp.migrations.decorations import comment_ownership_required
 from commentapp.migrations.forms import CommentCreationForm
 
 
@@ -21,5 +23,18 @@ class CommentCreateView(CreateView):
         temp_comment.save()
         return super().form_valid(form)
 
-    def get_success_url(selfself):
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
+
+    def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
