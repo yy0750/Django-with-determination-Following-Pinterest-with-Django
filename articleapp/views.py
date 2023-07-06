b2b53adfdf8f14e73992df6a6eb024f9caa2076c
yy0750 <yywls23@gmail.com>
2023-07-07 03:39:30 +0900

    django course 36 commit

--- articleapp/views.py
+++ articleapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorations import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -57,3 +57,10 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_artile'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 5
